af0cc52c6670a407e220c144d3078cd424e18315
Josh Cummings <josh.cummings@gmail.com>
2020-02-17 12:34:59 -0700

    Custom Login Page

--- keeper/src/main/java/io/jzheaux/springsecurity/keeper/SecurityConfig.java
+++ keeper/src/main/java/io/jzheaux/springsecurity/keeper/SecurityConfig.java
@@ -1,15 +1,21 @@
 package io.jzheaux.springsecurity.keeper;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
+@RestController
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -23,9 +29,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http
 			.authorizeRequests(a -> a
+				.mvcMatchers("/login").permitAll()
 				.anyRequest().authenticated())
-			.oauth2Login(Customizer.withDefaults())
+			.oauth2Login(o -> o.loginPage("/login"))
 			.logout(l -> l.logoutSuccessHandler(handler))
 			.csrf(c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+	}
+
+	@GetMapping("/login")
+	public void login(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ClientRegistration client = this.clients.findByRegistrationId(request.getServerName());
+		if (client == null) {
+			response.setStatus(401);
+		} else {
+			response.sendRedirect("/oauth2/authorization/" + client.getRegistrationId());
+		}
 	}
 }
