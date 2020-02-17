0068897ac338af8cbfdfdfbd327be8b20208ad9d
Josh Cummings <josh.cummings@gmail.com>
2020-02-17 12:19:16 -0700

    Multi-tenancy by token type

--- goal/src/main/java/io/jzheaux/springsecurity/goal/SecurityConfig.java
+++ goal/src/main/java/io/jzheaux/springsecurity/goal/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.function.Supplier;
 import javax.servlet.http.HttpServletRequest;
 
 import com.google.common.base.Suppliers;
+import com.nimbusds.jwt.JWTParser;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.server.resource.BearerTokenAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.authentication.OpaqueTokenAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.introspection.NimbusOpaqueTokenIntrospector;
@@ -32,13 +34,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	AuthenticationManagerResolver<HttpServletRequest> resolver() {
-		Supplier<AuthenticationManager> jwt = Suppliers.memoize(this::jwt);
-		return request -> jwt.get();
+		Supplier<AuthenticationManager> tokenType = Suppliers.memoize(this::tokenType);
+		return request -> tokenType.get();
+	}
+
+	AuthenticationManager tokenType() {
+		AuthenticationManager jwt = jwt();
+		AuthenticationManager opaqueToken = opaqueToken();
+		return authentication ->
+				isAJwt((BearerTokenAuthenticationToken) authentication) ?
+						jwt.authenticate(authentication) :
+						opaqueToken.authenticate(authentication);
+	}
+
+	boolean isAJwt(BearerTokenAuthenticationToken token) {
+		try {
+			JWTParser.parse(token.getToken());
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	AuthenticationManager jwt() {
 		JwtDecoder jwt = JwtDecoders.fromIssuerLocation("http://idp:9999/auth/realms/one");
 		JwtAuthenticationProvider provider = new JwtAuthenticationProvider(jwt);
+		return provider::authenticate;
+	}
+
+	AuthenticationManager opaqueToken() {
+		OpaqueTokenIntrospector introspector = new NimbusOpaqueTokenIntrospector(
+				"http://idp:9999/auth/realms/two/protocol/openid-connect/token/introspect",
+				"keeper",
+				"bfbd9f62-02ce-4638-a370-80d45514bd0a");
+		OpaqueTokenAuthenticationProvider provider = new OpaqueTokenAuthenticationProvider(introspector);
 		return provider::authenticate;
 	}
 
